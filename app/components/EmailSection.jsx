8c20df956ca076d048b2f219ed73f8a72178b80e
Prudence Levison <you@example.com>
2025-06-11 15:02:36 +0100

    started the contact us section

---
+++ app/components/EmailSection.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const EmailSection = () => {
+  return (
+    <section className='grid md:grid-cols-2 my-12 py-12 gap-4'>
+      <div>
+        <h5 className='text-xl font-bold text-white my-2'>Let's Connect</h5>
+        <p className='text-[#ADB7BE] mb-4 max-w-md'>
+          I'm on the lookout for new opportunities! Whether it's a question or just a quick hello, don’t hesitate to reach out — I’ll get back to you soon. 
+        </p>
+      </div>
+      <div className='socials flex flex-row'></div>
+    </section>
+  )
+}
+
+export default EmailSection
